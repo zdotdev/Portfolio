3562c38671541014a702c6230461ee81429e3c98
zed ragas <www.ragaszedrick25@gmail.com>
2023-09-16 14:18:52 +0800

    Co-authored-by: dimaliwatkent <dimaliwatkent@users.noreply.github.com>

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+
+  return (
+    <h1>Test</h1>
+    To do so, execute the “$ git config –global user. <name/password>” command. After the verification, run the “$ git config –global user.name <user-name>” command for username and the “$ git config –global user. password <user-password>” command for password configuration.
+  )
+}
+
+export default App
