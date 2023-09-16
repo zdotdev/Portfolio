b419cfc0d831aa56cdb20b36d83d8a11bfd9bf7f
Zed0825 <www.ragaszedrick25@gmail.com>
2023-09-16 21:55:02 +0800

    app.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,10 +2,8 @@ import { About, Contact, Hero, Navbar, Projects } from "./components"
 
 export default function App() {
   return (
-    <div><Navbar/>
-
-    <div className="h-[1000px]"></div>
-    <h1 className="text-red-500">test</h1>
+    <div><div className="fixed inset-0 flex justify-end text-white text-[24px] font-bold grid-cols-3 p-8 text-right table-auto"><Navbar/></div>
+    <div className="bg-gray-500 h-[1000px]"></div>
     <About/><Hero/><Projects/><Contact/></div>
   )
 }
